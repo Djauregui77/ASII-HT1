4490560eb13af0e1ac097884658a84713e24d27f
ditzepl <69169785+ditzepl@users.noreply.github.com>
2020-08-09 00:07:43 -0600

    Version 1 Sistema de cafeteria
    Se publica la versión 1 del sistema de cafeteria.

---
+++ ASII-HT1-1/src/test/java/gt/com/antiguaburguer/ASIIHT1/AsiiHt11ApplicationTests.java
@@ -0,0 +1,13 @@
+package gt.com.antiguaburguer.ASIIHT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiiHt11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
